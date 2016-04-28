4a52da8a365b6db4087f15c22139fd9d5bb1dd0f
mourmoux <a.cusset@gmail.com>
2016-04-28 16:33:52 +0000

    petits fix

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+        $(".button-collapse").sideNav();
+        $('.scrollspy').scrollSpy();
+        $('.collapsible').collapsible({
+            accordion: true
+        });
+        
+        $('#contact-modal').leanModal();
+        
+        var sliders = $('.sliders');
+        var slidersValue = [10, 20, 30, 40];
+        $.each(sliders, function (key, value) {
+            noUiSlider.create(value, {
+                start: slidersValue[key],
+                connect: "lower",
+                orientation: "horizontal",
+                range: {'min': 0, 'max': 100},
+                step: 1,
+                animate: false
+            });
+        });
+
+        $('nav').pushpin({
+            top: $('nav').offset().top
+        });
+        
+        $('.table-of-contents').pushpin({
+            top: $('nav').offset().top + $('nav').height(),
+            offset: $('nav').height()
+        });
+});
