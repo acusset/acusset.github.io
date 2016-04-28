ddce83346b69755512a98d4cc2d3d01266dfab5b
Antoine Cusset <a.cusset@gmail.com>
2016-04-28 21:57:45 +0200

    on avance !

--- js/script.js
+++ js/script.js
@@ -1,31 +1,34 @@
 $(document).ready(function () {
-        $(".button-collapse").sideNav();
-        $('.scrollspy').scrollSpy();
-        $('.collapsible').collapsible({
-            accordion: true
-        });
-        
-        $('#contact-modal').leanModal();
-        
-        var sliders = $('.sliders');
-        var slidersValue = [10, 20, 30, 40];
-        $.each(sliders, function (key, value) {
-            noUiSlider.create(value, {
-                start: slidersValue[key],
-                connect: "lower",
-                orientation: "horizontal",
-                range: {'min': 0, 'max': 100},
-                step: 1,
-                animate: false
-            });
-        });
+    
+    $(".button-collapse").sideNav();
+    
+    $('.scrollspy').scrollSpy();
+    
+    $('.collapsible').collapsible({
+        accordion: true
+    });
 
-        $('nav').pushpin({
-            top: $('nav').offset().top
+    $('.modal-trigger').leanModal();
+
+    var sliders = $('.sliders');
+    var slidersValue = [10, 20, 30, 40];
+    $.each(sliders, function (key, value) {
+        noUiSlider.create(value, {
+            start: slidersValue[key],
+            connect: "lower",
+            orientation: "horizontal",
+            range: {'min': 0, 'max': 100},
+            step: 1,
+            animate: false
         });
-        
-        $('.table-of-contents').pushpin({
-            top: $('nav').offset().top + $('nav').height(),
-            offset: $('nav').height()
-        });
+    });
+
+    $('nav').pushpin({
+        top: $('nav').offset().top
+    });
+
+    $('.table-of-contents').pushpin({
+        top: $('nav').offset().top + $('nav').height(),
+        offset: $('nav').height()
+    });
 });
